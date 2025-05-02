770d4a6909b10022129e0767164c8c9b5dfbc8b7
RennanRBS <rennanrbs@gmail.com>
2025-05-02 16:26:01 -0300

    first commit

---
+++ MB/MB/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MB
+//
+//  Created by Rennan Rebouças  on 02/05/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .red
+    }
+
+
+}
+
---
+++ MB/MBTests/MBTests.swift
@@ -0,0 +1,17 @@
+//
+//  MBTests.swift
+//  MBTests
+//
+//  Created by Rennan Rebouças  on 02/05/25.
+//
+
+import Testing
+@testable import MB
+
+struct MBTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
